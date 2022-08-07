bb40dc8bd92eb333d31fc5b2db1ca64c60eb4167
Kuria_dan <thuku.kuria@live.com>
2022-08-07 22:15:12 +0300

    print in and unsigned ints

--- print_numbers.c
+++ print_numbers.c
@@ -1,12 +1,62 @@
-#include "main.c"
+#include "main.h"
 /**
  * print_number - print Numbers
  *
- * @arg: num received by va_arg
+ * @args: num received by va_arg
  *
  * Return: length count
  */
-int print_number()
+int print_number(va_list args)
 {
+	int n;
+	int div;
+	int len;
+	unsigned int num;
 
+	n = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _putchar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div *= 10;
+	}
+	return (len);
+}
+/**
+  * print_unsigned_number - Prints an unsigned number
+  * @n: unsigned int to be printed
+  * Return: amount of numbers to be printed
+  */
+int print_unsigned_int(unsigned int n)
+{
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	for (; n / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (len);
 }
