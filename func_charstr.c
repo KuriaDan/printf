5752d9552744842dfeb36a70951f19e675d9204b
Kuria_dan <thuku.kuria@live.com>
2022-08-08 10:30:39 +0300

    base changes and handling str

--- func_charstr.c
+++ func_charstr.c
@@ -2,30 +2,45 @@
 /**
  * _print_char - prints character
  *
- * @c: receives character to be printed
+ * @list: receives character to be printed
  *
  * Return: 0 on SUCESSS -> length of char
  */
-int _print_char(char c)
+int _print_char(va_list list)
 {
-	_putchar(c);
+	_putchar(va_arg(list, int));
 	return (1);
 }
 /**
  * _print_string - Print string
  *
- * @s: receives string to be printed
+ * @list: receives string to be printed
  *
  * Return: 0 on SUCESSS
  */
-int _print_string(char *s)
+int _print_string(va_list list)
 {
-	int i = 0;
+	int i;
+	char *str;
 
-	for (; s[i] != '\0'; i++)
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 	}
 	return (i);
+}
+
+/**
+  * print_percent - prints a percent sybol
+  * @list: list of arguments
+  * Return: amount of characters printed
+  */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_putchar('%');
+	return (1);
 }
 
