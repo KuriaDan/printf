9dd5e3aba9d1e44973967e2a8d404e1a7a94702a
Kuria_dan <thuku.kuria@live.com>
2022-08-08 10:28:25 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -1,41 +1,41 @@
 /**
  * File -_printf.c
  * Author:
- * 	 Bezawork Lindlof
- * 	 KuriaDan
+ *	 Bezawork Lindlof
+ *	 KuriaDan
  */
 #include <stdarg.h>
-#include <stdlib.>
+#include <stdlib.h>
 #include "main.h"
 /**
  * _printf - counts number of characters
- *
  * @format: Format of what needs to be printed
- *
- * Return:int
+ * Return: int or chars
  */
 int _printf(const char *format, ...)
 {
+	int printed_chars;
 	va_list args;
-	locate print_list[] = {
+	locate list[] = {
 		{"c", _print_char},
 		{"s", _print_string},
 		{"d", _print_integer},
-		{"i",_print_integer},
+		{"i", _print_integer},
 		{"b", _print_binary},
-		{"u", _print_unsignedint},
-		{"o",_print_octal},
-		{"x", print_hex},
-		{"X", _print_HEX},
-		{"r", _print_reversed},
-		{"R", _print_rot13},
-		{NULL, NULL}	
+		{"u", unsigned_integer},
+		{"o", _print_octal},
+		{"x", _print_hex},
+		{"X", _print_heX},
+		{"r", print_rev},
+		{"R", rot13},
+		{NULL, NULL}
 	};
-	
 	if (format == NULL)
 	{
 		return (-1);
 	}
 	va_start(args, format);
-	va_end
+	printed_chars = parser(format, list, args);
+	va_end(args);
+	return (printed_chars);
 }
